0391a03d63bbb865e77f2226a9928e00e152dcd0
eastfire <eastfire_he@hotmail.com>
2016-06-03 11:23:34 +0800

    Update character.js

--- src/model/character.js
+++ src/model/character.js
@@ -3,12 +3,34 @@ var CharacterModel = Backbone.Model.extend({
         return {
             hp: 100,
             maxHp: 100,
+            mana: 100,
+            maxMana: 100,
 
             exp: 0,
             requiredExp: 100,
-            cunning: 0,
-            constitute: 10,
-            luck: 0
+            
+            attack: 10,
+            defense: 10,
+            
+            strength: 0, // effect attack
+            cunning: 0, //exp required
+            constitute: 10, //maxhp
+            luck: 0, //item drop
+            dexterity: 0, // card speed + dodge attack
+            stemina: 3, //card number + card appear duration
+            
+            earthResist: 0,
+            waterResist: 0,
+            airResist: 0,
+            fireResist: 0,
+            lightResist: 0,
+            darkResist: 0,
+            
+            deck: [],
+            discardDeck: [],
+            
+            equipments: [],
+            
         }
     },
     initialize:function(){
